9841d6a52030722493b05150fb8f6e4d1e5b229a
Matthew Simo <matthew.a.simo@gmail.com>
2012-06-13 14:35:45 -0400

    Initial commit for plugin branch for issue press.

---
+++ IssuePress.php
@@ -0,0 +1,10 @@
+<?php
+/*
+Plugin Name: IssuePress
+Plugin URI: http://upthemes.com
+Description: Github Issues integration with WP - for support
+Version: 0.0.0
+Author: Upthemes
+Author URI: http://upthemes.com/ 
+*/
+
--- config.rb
+++
@@ -1,9 +0,0 @@
-http_path = "/"
-css_dir = "assets/css"
-sass_dir = "assets/scss"
-images_dir = "assets/img"
-javascripts_dir = "assets/js"
-
-output_style = :compressed
-# relative_assets = true
-line_comments = false
