cd5867f38cda39edd20d26df131c68efe8baf485
Anthony Garand <anthonygarand@gmail.com>
2013-04-09 11:20:03 -0400

    Added styles for comment thread

--- header.php
+++ header.php
@@ -1,14 +1,14 @@
 <!doctype html>
 <html>
 <head>
-  <title></title>
+  <title>IssuePress</title>
   <meta name="viewport" content="width=device-width,initial-scale=1">
   <link rel="stylesheet" href="/assets/css/main.css">
+  <script type="text/javascript" src="//use.typekit.net/gey4jrw.js"></script>
+<script type="text/javascript">try{Typekit.load();}catch(e){}</script>
   <!--[if lt IE 9]><script src="//html5shiv.googlecode.com/svn/trunk/html5.js"></script><![endif]-->
   <script type="text/javascript" src="/assets/js/jquery.min.js"></script>
   <script type="text/javascript" src="/assets/js/main.js"></script>
 </head>
 <body>
   
-</body>
-</html>
---
+++ index.php
@@ -0,0 +1,284 @@
+<?php include("header.php"); ?>
+
+<div class="content">
+
+  <header>
+    <a href="" class="logo">IssuePress</a>
+    <nav>
+      <ul>
+        <li><a href="">Dashboard</a></li>
+        <li><a href="">Support Sections</a></li>
+        <li><a href="">Create New Issue</a></li>
+        <li class="account">
+          <a href=""><img src="https://fbcdn-profile-a.akamaihd.net/hprofile-ak-prn1/157519_1407402929_769495043_q.jpg"/>Anthony Garand</a>
+          <ul>
+            <li><a href="">Dashboard</a></li>
+            <li><a href="">Support Sections</a></li>
+            <li><a href="">Create New Issue</a></li>
+            <li><a href="">Dashboard</a></li>
+            <li><a href="">Support Sections</a></li>
+          </ul>
+        </li>
+      </ul>
+    </nav>
+  </header>
+
+  <div class="left-column">
+
+    <section class="breadcrumb">
+      <a href="">Support</a>
+      <a href="">Garage Band</a>
+      <a href="">Music Player won't work</a>
+    </section>
+
+    <section class="search">
+      <form>
+        <input type="text" class="search-query" placeholder="Search"/>
+      </form>
+    </section>
+
+    <section class="message">
+      <div class="message-title">
+        Welcome to UpThemes Support
+      </div>
+      <div class="message-content">
+        Donec id elit non mi porta gravida at eget metus. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Vestibulum id ligula porta felis euismod semper.
+      </div>
+    </section>
+
+    <section class="recent-activity">
+      <div class="section-title">
+        Recent Activity on Tickets you are Following
+      </div>
+      <div class="recent-activity-list">
+        <div class="recent-activity-item">
+          <div class="recent-activity-content">
+            <a href="">imbradmiller</a> commented on issue <a href="">#421</a>
+          </div>
+          <a href="" class="recent-activity-timeago">4 hours ago</a>
+        </div>
+        <div class="recent-activity-item">
+          <div class="recent-activity-content">
+            <a href="">imbradmiller</a> commented on issue <a href="">#421</a>
+          </div>
+          <a href="" class="recent-activity-timeago">4 hours ago</a>
+        </div>
+        <div class="recent-activity-item">
+          <div class="recent-activity-content">
+            <a href="">imbradmiller</a> commented on issue <a href="">#421</a>
+          </div>
+          <a href="" class="recent-activity-timeago">4 hours ago</a>
+        </div>
+        <div class="recent-activity-item">
+          <div class="recent-activity-content">
+            <a href="">imbradmiller</a> commented on issue <a href="">#421</a>
+          </div>
+          <a href="" class="recent-activity-timeago">4 hours ago</a>
+        </div>
+        <div class="recent-activity-item">
+          <div class="recent-activity-content">
+            <a href="">imbradmiller</a> commented on issue <a href="">#421</a>
+          </div>
+          <a href="" class="recent-activity-timeago">4 hours ago</a>
+        </div>
+      </div>
+    </section>
+
+    <section class="issue-thread">
+      <div class="comment">
+        <div class="comment-meta">
+          <a href="" class="author">
+            <img src="https://fbcdn-profile-a.akamaihd.net/hprofile-ak-prn1/157519_1407402929_769495043_q.jpg"/>
+            <span class="author-name">garand</span>
+          </a>
+          <span class="author-action">created an issue</span>
+          <a href="" class="comment-timeago">1 month ago</a>
+        </div>
+        <div class="comment-content">
+          <div class="comment-title">Music Player won't work</div>
+          <p>Vestibulum id ligula porta felis euismod semper. Maecenas sed diam eget risus varius blandit sit amet non magna. Integer posuere erat a ante venenatis dapibus posuere velit aliquet. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor.</p>
+          <div class="comment-tags">
+            <a href="">Music</a>
+            <a href="">Player</a>
+            <a href="">Pissed</a>
+          </div>
+          <a href="" class="follow-thread">Follow Thread</a>
+        </div>
+      </div>
+      <div class="comment">
+        <div class="comment-meta">
+          <a href="" class="author">
+            <img src="https://fbcdn-profile-a.akamaihd.net/hprofile-ak-prn1/157519_1407402929_769495043_q.jpg"/>
+            <span class="author-name">garand</span>
+            <span class="author-action">said</span>
+          </a>
+          <a href="" class="comment-timeago">1 month ago</a>
+        </div>
+        <div class="comment-content">
+          <p>Vestibulum id ligula porta felis euismod semper. Maecenas sed diam eget risus varius blandit sit amet non magna. Integer posuere erat a ante venenatis dapibus posuere velit aliquet. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor.</p>
+        </div>
+      </div>
+      <div class="comment staff">
+        <div class="comment-meta">
+          <a href="" class="author">
+            <img src="https://fbcdn-profile-a.akamaihd.net/hprofile-ak-prn1/157519_1407402929_769495043_q.jpg"/>
+            <span class="author-name">garand</span>
+            <span class="author-action">said</span>
+          </a>
+          <a href="" class="comment-timeago">1 month ago</a>
+        </div>
+        <div class="comment-content">
+          <p>Vestibulum id ligula porta felis euismod semper. Maecenas sed diam eget risus varius blandit sit amet non magna. Integer posuere erat a ante venenatis dapibus posuere velit aliquet. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor.</p>
+        </div>
+      </div>
+      <div class="comment">
+        <div class="comment-meta">
+          <a href="" class="author">
+            <img src="https://fbcdn-profile-a.akamaihd.net/hprofile-ak-prn1/157519_1407402929_769495043_q.jpg"/>
+            <span class="author-name">garand</span>
+            <span class="author-action">said</span>
+          </a>
+          <a href="" class="comment-timeago">1 month ago</a>
+        </div>
+        <div class="comment-content">
+          <p>Vestibulum id ligula porta felis euismod semper. Maecenas sed diam eget risus varius blandit sit amet non magna. Integer posuere erat a ante venenatis dapibus posuere velit aliquet. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor.</p>
+        </div>
+      </div>
+      <form class="comment new-comment">
+        <div class="comment-meta">
+          <a href="" class="author">
+            <img src="https://fbcdn-profile-a.akamaihd.net/hprofile-ak-prn1/157519_1407402929_769495043_q.jpg"/>
+            <span class="author-action">Logged in as</span>
+            <span class="author-name">garand</span>
+          </a>
+          <a href="" class="comment-timeago">Logout</a>
+        </div>
+        <div class="comment-content">
+          <textarea></textarea>
+        </div>
+        <input type="submit" class="button" value="Post Comment"/>
+      </form>
+    </section>
+
+    <section class="issue-list">
+      <div class="section-title">
+        Recently Updated Tickets
+      </div>
+      <div class="issue-list-item">
+        <div class="issue-title">
+          Issue with the Music Player for the homepage on Garage Band
+        </div>
+        <div class="issue-date">
+          Last Updated: December 27, 2012
+        </div>
+        <div class="issue-description">
+          Maecenas sed diam eget risus varius blandit sit amet non magna. Cras mattis consectetur purus sit amet fermentum. Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum. Integer posuere erat a ante venenatis dapibus posuere velit aliquet. Etiam porta sem malesuada magna mollis euismod. Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor.
+        </div>
+        <a href="" class="issue-link">Open Issue</a>
+      </div>
+      <div class="issue-list-item">
+        <div class="issue-title">
+          Issue with the Music Player for the homepage on Garage Band
+        </div>
+        <div class="issue-date">
+          Last Updated: December 27, 2012
+        </div>
+        <div class="issue-description">
+          Maecenas sed diam eget risus varius blandit sit amet non magna. Cras mattis consectetur purus sit amet fermentum. Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum. Integer posuere erat a ante venenatis dapibus posuere velit aliquet. Etiam porta sem malesuada magna mollis euismod. Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor.
+        </div>
+        <a href="" class="issue-link">Open Issue</a>
+      </div>
+      <div class="issue-list-item">
+        <div class="issue-title">
+          Issue with the Music Player for the homepage on Garage Band
+        </div>
+        <div class="issue-date">
+          Last Updated: December 27, 2012
+        </div>
+        <div class="issue-description">
+          Maecenas sed diam eget risus varius blandit sit amet non magna. Cras mattis consectetur purus sit amet fermentum. Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum. Integer posuere erat a ante venenatis dapibus posuere velit aliquet. Etiam porta sem malesuada magna mollis euismod. Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor.
+        </div>
+        <a href="" class="issue-link">Open Issue</a>
+      </div>
+      <div class="issue-list-item">
+        <div class="issue-title">
+          Issue with the Music Player for the homepage on Garage Band
+        </div>
+        <div class="issue-date">
+          Last Updated: December 27, 2012
+        </div>
+        <div class="issue-description">
+          Maecenas sed diam eget risus varius blandit sit amet non magna. Cras mattis consectetur purus sit amet fermentum. Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum. Integer posuere erat a ante venenatis dapibus posuere velit aliquet. Etiam porta sem malesuada magna mollis euismod. Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor.
+        </div>
+        <a href="" class="issue-link">Open Issue</a>
+      </div>
+      <div class="issue-list-item">
+        <div class="issue-title">
+          Issue with the Music Player for the homepage on Garage Band
+        </div>
+        <div class="issue-date">
+          Last Updated: December 27, 2012
+        </div>
+        <div class="issue-description">
+          Maecenas sed diam eget risus varius blandit sit amet non magna. Cras mattis consectetur purus sit amet fermentum. Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum. Integer posuere erat a ante venenatis dapibus posuere velit aliquet. Etiam porta sem malesuada magna mollis euismod. Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor.
+        </div>
+        <a href="" class="issue-link">Open Issue</a>
+      </div>
+    </section>
+
+  </div>
+
+  <div class="right-column">
+
+    <section class="support-sections">
+      <div class="section-title">
+        All Support Sections
+      </div>
+      <div class="section-nav">
+        <a href="">Most Recent</a>
+        <a href="">All</a>
+      </div>
+      <a href="" class="support-section">
+        <div class="support-section-title">GarageBand Theme</div>
+        <div class="support-section-date">December 27th, 2012</div>
+      </a>
+      <a href="" class="support-section">
+        <div class="support-section-title">GarageBand Theme</div>
+        <div class="support-section-date">December 27th, 2012</div>
+      </a>
+      <a href="" class="support-section">
+        <div class="support-section-title">GarageBand Theme</div>
+        <div class="support-section-date">December 27th, 2012</div>
+      </a>
+      <a href="" class="support-section">
+        <div class="support-section-title">GarageBand Theme</div>
+        <div class="support-section-date">December 27th, 2012</div>
+      </a>
+    </section>
+
+    <section class="tickets-following">
+      <div class="section-title">
+        Tickets I'm Following
+      </div>
+      <a href="" class="ticket">
+        <span class="ticket-title">Issue with the Music Player for the homepage on Garage Band</span>
+        <span class="ticket-meta">imbradmiller said an hour ago</span>
+        <span class="ticket-comment">This is exactly the issue I'm having and I fixed it by</span>
+      </a>
+      <a href="" class="ticket">
+        <span class="ticket-title">Issue with the Music Player for the homepage on Garage Band</span>
+        <span class="ticket-meta">imbradmiller said an hour ago</span>
+        <span class="ticket-comment">This is exactly the issue I'm having and I fixed it by</span>
+      </a>
+      <a href="" class="ticket">
+        <span class="ticket-title">Issue with the Music Player for the homepage on Garage Band</span>
+        <span class="ticket-meta">imbradmiller said an hour ago</span>
+        <span class="ticket-comment">This is exactly the issue I'm having and I fixed it by</span>
+      </a>
+    </section>
+
+  </div>
+
+</div>
+
+<?php include("footer.php"); ?>
