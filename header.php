34920da58e09b77d435b5b0d7050552c0be0eec2
Anthony Garand <anthonygarand@gmail.com>
2013-04-23 16:34:18 -0400

    Fixed indentation

--- header.php
+++ header.php
@@ -5,7 +5,7 @@
   <meta name="viewport" content="width=device-width,initial-scale=1">
   <link rel="stylesheet" href="/assets/css/main.css">
   <script type="text/javascript" src="//use.typekit.net/gey4jrw.js"></script>
-<script type="text/javascript">try{Typekit.load();}catch(e){}</script>
+  <script type="text/javascript">try{Typekit.load();}catch(e){}</script>
   <!--[if lt IE 9]><script src="//html5shiv.googlecode.com/svn/trunk/html5.js"></script><![endif]-->
   <script type="text/javascript" src="/assets/js/jquery.min.js"></script>
   <script type="text/javascript" src="/assets/js/main.js"></script>
