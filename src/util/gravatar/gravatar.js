702a8a9be78c0d60b51438ef811661fa056693b2
Matthew Simo <matthew.a.simo@gmail.com>
2013-07-25 16:38:31 -0500

    Overhaul the gravatar directive I found to be not a piece of crap

--- src/util/gravatar/gravatar.js
+++ src/util/gravatar/gravatar.js
@@ -1,55 +1,34 @@
 'use strict';
 
-/*
- * An Simple AngularJS Gravatar Directive
- *
- * Written by Jim Lavin
- * http://codingsmackdown.tv
- *
- */
-
 angular.module('ui.gravatar', ['md5']).
     directive('gravatarImage', ['md5', function (md5) {
         return {
             restrict:"A",
+            replace: true,
             scope: {
+              secure: '@secure',
               email: '@email',
-              size: '@size',
-              rating: '@rating',
-              default: '@default'
+              s: '@s',
+              r: '@r',
+              d: '@d'
             },
-            link:function (scope, elm, attrs) {
-                // by default the values will come in as undefined so we need to setup a
-                // watch to notify us when the value changes
-                scope.$watch(attrs.email, function (value) {
-                    // let's do nothing if the value comes in empty, null or undefined
-                    if ((value !== null) && (value !== undefined) && (value !== '') && (null != value.match(/.*@.*\..{2}/))) {
-                        // convert the value to lower case and then to a md5 hash
-                        var hash = md5.createHash(value.toLowerCase());
-                        // parse the size attribute
-                        var size = attrs.size;
-                        // default to 40 pixels if not set
-                        if((size=== null) || (size == undefined) || (size == '')){
-                            size = 40;
-                        }
-                        // parse the ratings attribute
-                        var rating = attrs.rating;
-                        // default to pg if not set
-                        if((rating === null) || (rating === undefined)|| (rating === '')){
-                            rating = 'pg';
-                        }
-                        // parse the default image url
-                        var defaultUrl = attrs.default;
-                        if((defaultUrl === null) || (defaultUrl === undefined)|| (defaultUrl === '')) {
-                            defaultUrl = '404';
-                        }
-                        // construct the tag to insert into the element
-                        var tag = '<img class="gravatar-icon" src="' + (attrs.secure ? 'https://secure' : 'http://www' ) + '.gravatar.com/avatar/' + hash + '?s=' + size + '&r=' + rating + '&d=' + defaultUrl + '" >'
-                        //remove any existing imgs 
-                         $(elm).find(".gravatar-icon").remove();           
-                        // insert the tag into the element
-                        elm.append(tag);
-                    }
-                });
-            }};
+            template: '<img src="{{src}}" />',
+            controller: ['$scope', '$element', '$attrs', function($scope, $element, $attrs){
+              var emailHash = md5.createHash($scope.email);
+              var protocol = $scope.secure ? 'https://secure' : 'http://';
+              var src = protocol + 'gravatar.com/avatar/' + emailHash + '?';
+              
+              if(typeof $scope.s !== 'undefined') 
+                src += 's=' + $scope.s + '&';
+                
+              if(typeof $scope.r !== 'undefined')  
+                src += 'r=' + $scope.r + '&';
+
+              if(typeof $scope.d !== 'undefined')
+                src += 'd=' + encodeURIComponent($scope.d);
+
+              $scope.src = src;
+            }],
+            
+        };
     }]);
