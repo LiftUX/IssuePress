11dad2162bc7373e1d082581cd2bff357a6a6e03
Matthew Simo <matthew.a.simo@gmail.com>
2013-08-05 10:54:15 -0500

    Output the list of selected sections on section template. Add component controller & tpl

--- src/app/components/breadcrumbs.js
+++ src/app/components/breadcrumbs.js
@@ -5,7 +5,7 @@ angular.module('components.breadcrumbs', []).directive('ipBreadcrumbs', function
     replace: true,
     template: 
       '<section class="breadcrumb">' +
-        '<a href="">Support</a>' +
+        '<a href="">Sections</a>' +
         '<a href="">Garage Band</a>' +
         '<a href="">Music Player won\'t work</a>' + 
       '</section>'
--- src/app/sections/sections.js
+++ src/app/sections/sections.js
@@ -3,4 +3,6 @@ angular.module('sections', [])
 
 .controller('SectionsCtrl', ['$scope', '$location', function($scope, $location) {
   
+  $scope.sections = IP_repos;
+
 }]);
