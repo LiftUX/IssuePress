d8d70a7e2637a6273722e33f8981b131c053176f
Matthew Simo <matthew.a.simo@gmail.com>
2013-08-27 16:16:31 -0500

    Add some data from the api to use for building out the recent activity module/widget thing.

--- src/app/repo/repo.js
+++ src/app/repo/repo.js
@@ -1,9 +1,28 @@
 
-angular.module('repo', [])
+angular.module('repo', ['AppState'])
 
-.controller('RepoCtrl', ['$scope', '$location', '$routeParams', function($scope, $location, $routeParams) {
+.controller('RepoCtrl', ['$scope', '$location', '$routeParams', '$http', 'IPAppState', function($scope, $location, $routeParams, $http, IPAppState) {
   
-  console.log($location.path());
+  var ipUrl = IPAppState.IP_API_PATH;
+  var repo = $routeParams.repo
 
-  console.log($routeParams);
+  $http({
+    method: 'GET',
+    url: ipUrl + repo
+  }).success(function(data, status, headers, config){
+    if(status === 200){
+      var repoData = data.data; 
+      $scope.repo = repoData.repo;
+      $scope.activity = repoData.activity;
+      $scope.issues = repoData.issues;
+      $scope.releases = repoData.releases;
+    }
+    console.log("Success");
+    console.log(repoData);
+  }).error(function(data, status, headers, config){
+    console.log("Fail");
+    console.log(data);
+    console.log(status);
+  });
+
 }]);
