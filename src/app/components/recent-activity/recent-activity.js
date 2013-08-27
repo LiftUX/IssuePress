d3c3a5ac19e0386f48454c2c7da6f1b5ae999533
Matthew Simo <matthew.a.simo@gmail.com>
2013-08-27 16:14:07 -0500

    Chage up the recent activity item directive, add directives for recent activity item title & meta. Transclude them so we can pass in whatever we want and order them however we like. Make recent activity directive use timeago filter for it's time field. Make  recent activity directives more flexible.

--- src/app/components/recent-activity/recent-activity.js
+++ src/app/components/recent-activity/recent-activity.js
@@ -19,11 +19,34 @@ angular.module('components.recentActivity', [])
     replace: true,
     transclude: true,
     scope: {
-      'title': '@title',
       'icon': '@icon',
       'timeago': '@timeago',
-      'meta': '@meta',
+      'href': '@href'
     },
     templateUrl: IP_PATH + '/app/components/recent-activity/recent-activity-item.tpl.html'
   }
-});
+})
+
+.directive('ipRecentActivityItemTitle', function(){
+  return {
+    restrict: 'A',
+    replace: true,
+    transclude: true,
+    scope: {
+      'href': '@href',
+    },
+    template: '<a href="{{href}}" class="recent-activity-title"><div data-ng-transclude></div></a>'
+  }
+})
+
+
+.directive('ipRecentActivityItemMeta', function(){
+  return {
+    restrict: 'A',
+    replace: true,
+    transclude: true,
+    scope: {
+    },
+    template: '<div class="recent-activity-meta"><div data-ng-transclude></div></div>'
+  }
+})
