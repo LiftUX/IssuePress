6f7a38e12eda020ad6e82bafc974c122bb9ba4a0
Matthew Simo <matthew.a.simo@gmail.com>
2013-08-28 17:43:21 -0500

    Hammer out some issue template related changes. Add in call to WP backend. Tie up data in issue template file. Modify directive usage. etc...

--- src/app/issue/issue.js
+++ src/app/issue/issue.js
@@ -1,10 +1,32 @@
 
-angular.module('issue', [])
+angular.module('issue', ['AppState', 'user', 'ui.gravatar'])
 
-.controller('IssueCtrl', ['$scope', '$location', '$routeParams', function($scope, $location, $routeParams) {
+.controller('IssueCtrl', ['$scope', '$location', '$routeParams', '$http', 'IPAppState', 'IPUser', 'gravatar',
+function($scope, $location, $routeParams, $http, IPAppState, IPUser, gravatar) {
+
+  $scope.user = IPUser.user;
+  $scope.login_link = IPUser.login_link;
+  $scope.logout_link = IPUser.logout_link;
+
+
+  var ipUrl = IPAppState.IP_API_PATH;
   $scope.params = $routeParams;
-  
-  console.log($location.path());
 
-  console.log($routeParams);
+  $http({
+    method: 'GET',
+    url: ipUrl + $scope.params.repo + '/' + $scope.params.issue
+  }).success(function(data, status, headers, config){
+    if(status === 200){
+      var issueData = data.data; 
+      $scope.issue = issueData.issue;
+      $scope.comments = issueData.comments;
+    }
+    console.log("Success");
+    console.log(data);
+  }).error(function(data, status, headers, config){
+    console.log("Fail");
+    console.log(data);
+    console.log(status);
+  });
+  
 }]);
