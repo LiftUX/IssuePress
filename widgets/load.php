b2b26b7cac7ec60ca7ada74c21f7a68e0119d26e
Matthew Simo <matthew.a.simo@gmail.com>
2013-09-04 13:33:16 -0500

    Registration file for release widget

--- widgets/load.php
+++ widgets/load.php
@@ -10,4 +10,5 @@ include_once('message/ip-message.php');
 include_once('sections/ip-sections.php');
 include_once('recent-comments/ip-recent-comments.php');
 include_once('recent-issues/ip-recent-issues.php');
+include_once('release/ip-release.php');
 
---
+++ widgets/release/ip-release.php
@@ -0,0 +1,56 @@
+<?php
+if(!class_exists('ip_release')){
+  class ip_release extends IP_Widget{
+    protected $fields = array(
+      'title' => 'Title',
+    );
+
+    public function __construct(){
+      parent::__construct(
+        'ip_release',
+        'IP Recent Release',
+        array('description' => __('Displays the most recent release for each section, or if placed in a section, specifically for that section. For use on IssuePress Sidebars only.', 'IssuePress'))
+      );
+    }
+
+    public function widget($args, $instance){
+      extract($args);
+      extract($instance);
+
+      echo $before_widget;
+
+      $title = apply_filters('widget_title', $title);
+      if($title)
+        echo $before_title . $title . $after_title;
+
+      echo $after_widget;
+    }
+
+    public function update($new_instance, $old_instance){
+      return $new_instance;
+    }
+
+    public function form($instance){
+      $form = '';
+
+      foreach($this->fields as $id => $label){
+        $field_id = $this->get_field_id($id);
+        $field_name = $this->get_field_name($id);
+
+        if(isset($instance[$id]))
+          $instance_var = $instance[$id];
+        else
+          $instance_var = '';
+
+        $form .= "<p><label for='$field_id'>$label</label>";
+
+        $form .= "<input class='widefat' id='$field_id' name='$field_name' type='text' value=\"$instance_var\">";
+
+        $form .= "</p>";
+
+      }
+      echo $form;
+    }
+  }
+  add_action('widgets_init', create_function('', 'register_widget( "ip_release" );'));
+}
