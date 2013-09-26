09acbbbfb893a8f4ae113c77a0c53716859b448d
Matthew Simo <matthew.a.simo@gmail.com>
2013-09-26 09:24:37 -0500

    Factory for app data in app state module.

--- src/app/app-state/app-state.js
+++ src/app/app-state/app-state.js
@@ -3,4 +3,12 @@ angular.module('AppState', [])
 .factory('IPAppState', function(){
   var appState = window.IP_Vars;
   return appState;
+})
+
+
+.factory('IPData', function(){
+
+  var data = {};
+  return data;
+  
 });
