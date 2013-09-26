1ef88af35fe53b7fd52c37d3486d401025487ba6
Matthew Simo <matthew.a.simo@gmail.com>
2013-09-26 09:07:20 -0500

    Remove call to previously removed widget in widgets/load.php

--- widgets/load.php
+++ widgets/load.php
@@ -9,7 +9,6 @@ include_once('base/ip-base.php');
 include_once('message/ip-message.php');
 include_once('sections/ip-sections.php');
 include_once('recent-comments/ip-recent-comments.php');
-include_once('recent-issues/ip-recent-issues.php');
 include_once('release/ip-release.php');
 include_once('recent-activity/ip-recent-activity.php');
 
