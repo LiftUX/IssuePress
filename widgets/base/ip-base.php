444499f09303c37cb4e4d82af224183d8ccd3411
Matthew Simo <matthew.a.simo@gmail.com>
2014-03-06 16:55:41 -0600

    Clean up widget code.

--- widgets/base/ip-base.php
+++ widgets/base/ip-base.php
@@ -1,5 +1,10 @@
 <?php
 if(!class_exists('IP_Widget')){
   class IP_Widget extends WP_Widget{
+
+    public function update($new_instance, $old_instance){
+      return $new_instance;
+    }
+
   }
 }
