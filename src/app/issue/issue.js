f4534c406c04b0b31a9c26960df5b4e4c3897932
Matthew Simo <matthew.a.simo@gmail.com>
2014-05-09 10:37:46 -0500

    Test for valid repo in the issue page controller - redirect to 404 if not.

--- src/app/issue/issue.js
+++ src/app/issue/issue.js
@@ -4,6 +4,12 @@ angular.module('issue', ['AppState', 'user'])
 .controller('IssueCtrl', ['$scope', '$location', '$routeParams', '$http', 'IPAppState', 'IPData', 'IPUser',
 function($scope, $location, $routeParams, $http, IPAppState, IPData, IPUser) {
 
+  var repo = $routeParams.repo;
+  // Test if repo is a valid name, otherwise goto 404
+  if(!IPAppState.isIPRepo(repo)) {
+    $location.path('/404');
+  }
+
   $scope.user = IPUser.user;
   $scope.login_link = IPUser.login_link;
   $scope.logout_link = IPUser.logout_link;
@@ -20,7 +26,7 @@ function($scope, $location, $routeParams, $http, IPAppState, IPData, IPUser) {
 
   $scope.fetchData = function() {
     // Set Data for this Scope from IPData service - fetch from cache, or from API otherwise
-    IPData.getIssueData($routeParams.repo, $routeParams.issue).then(function(data){
+    IPData.getIssueData(repo, $routeParams.issue).then(function(data){
       $scope.issue = data.issue;
       $scope.comments = data.comments;
     });
