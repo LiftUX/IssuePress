87e3e0c89617371febfebae4cfa08dc5f4791ac9
Matthew Simo <matthew.a.simo@gmail.com>
2014-05-22 16:48:14 -0500

    Add color manipulation helper function.

--- IP_helpers.php
+++ IP_helpers.php
@@ -5,6 +5,33 @@
  * @return plugin version
  */
 
+function lighten_color($hex, $steps) {
+    // Steps should be between -255 and 255. Negative = darker, positive = lighter
+    $steps = max(-255, min(255, $steps));
+
+    // Format the hex color string
+    $hex = str_replace('#', '', $hex);
+    if (strlen($hex) == 3) {
+        $hex = str_repeat(substr($hex,0,1), 2).str_repeat(substr($hex,1,1), 2).str_repeat(substr($hex,2,1), 2);
+    }
+
+    // Get decimal values
+    $r = hexdec(substr($hex,0,2));
+    $g = hexdec(substr($hex,2,2));
+    $b = hexdec(substr($hex,4,2));
+
+    // Adjust number of steps and keep it inside 0 to 255
+    $r = max(0,min(255,$r + $steps));
+    $g = max(0,min(255,$g + $steps));  
+    $b = max(0,min(255,$b + $steps));
+
+    $r_hex = str_pad(dechex($r), 2, '0', STR_PAD_LEFT);
+    $g_hex = str_pad(dechex($g), 2, '0', STR_PAD_LEFT);
+    $b_hex = str_pad(dechex($b), 2, '0', STR_PAD_LEFT);
+
+    return '#'.$r_hex.$g_hex.$b_hex;
+}
+
 function issuepress_get_version() {
   $plugin_data = get_plugin_data( __FILE__ );
   $plugin_version = $plugin_data['Version'];
