e00b2fe2fef9b10431816d0cdc542d6f61367bdb
Matthew Simo <matthew.a.simo@gmail.com>
2014-09-08 17:20:31 -0500

    Add load_settings method to public ip class.

--- public/class-issuepress-public.php
+++ public/class-issuepress-public.php
@@ -188,6 +188,23 @@ class IssuePress_Public {
 
 	}
 
+	/**
+	 * Loads tabs settings from DB into their own arrays.
+	 *
+	 * @since		1.0.0
+	 */
+	public function load_settings(){
+
+		$this->settings = (array) get_option( $this->plugin->get_options_key() );
+
+		$this->settings = array_merge( array(
+			'ip_test_setting' => '',
+			'ip_license_key'  => '',
+		), $this->settings );
+
+	}
+
+
 
 	/**
 	 * Register the stylesheets for the public-facing side of the site.
