9da1808f13faafc5c6f3f9efa6ef7a27289dc073
Matthew Simo <matthew.a.simo@gmail.com>
2014-10-06 08:43:19 -0500

    Define two php constants, plugin dir path & basename. Implement for includes.

--- issuepress.php
+++ issuepress.php
@@ -30,30 +30,33 @@ if ( ! defined( 'WPINC' ) ) {
 	die;
 }
 
+define( 'ISSUEPRESS_PLUGIN_DIR', plugin_dir_path( __FILE__ ) );
+define( 'ISSUEPRESS_PLUGIN_BASENAME', plugin_basename( __FILE__ ) );
+
 /**
  * The code that runs during plugin activation.
  * It's action is documented in includes/class-issuepress-activator.php 
  */
-require_once plugin_dir_path( __FILE__ ) . 'includes/class-issuepress-activator.php';
+require_once ISSUEPRESS_PLUGIN_DIR . 'includes/class-issuepress-activator.php';
 register_activation_hook( __FILE__, array( 'IssuePress_Activator', 'activate' ) );
 
 /**
  * The code that runs during plugin deactivation.
  * It's action is documented in includes/class-issuepress-deactivator.php
  */
-require_once plugin_dir_path( __FILE__ ) . 'includes/class-issuepress-deactivator.php';
+require_once  ISSUEPRESS_PLUGIN_DIR . 'includes/class-issuepress-deactivator.php';
 register_deactivation_hook( __FILE__, array( 'IssuePress_Deactivator', 'deactivate' ) );
 
 /**
  * The core plugin class that is used to define internationalization,
  * dashboard-specific hooks, and public-facing site hooks.
  */
-require_once plugin_dir_path( __FILE__ ) . 'includes/class-issuepress.php';
+require_once ISSUEPRESS_PLUGIN_DIR . 'includes/class-issuepress.php';
 
 /**
  * The extension parent class, used for easy extension creation.
  */
-include_once( plugin_dir_path( __FILE__ ) . 'includes/class-issuepress-extension.php' );
+include_once( ISSUEPRESS_PLUGIN_DIR . 'includes/class-issuepress-extension.php' );
 
 /**
  * Begins execution of the plugin.
